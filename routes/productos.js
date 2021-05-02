9283976f3989cd3a6fb7e1f5ed246c8f4a710941
isabelyb <isabelcristinayepesb@gmail.com>
2021-05-02 14:45:21 -0500

    create products route

--- routes/productos.js
+++ routes/productos.js
@@ -1,3 +1,23 @@
 const express = require('express');
-const {datos} = require{'ruta de datos'};
+const {productsMock} = require('../utils/mock_products.json');
+
+function productsApi(app) {
+    const router = express.Router();
+    app.user('/api/products', router);
+
+    router.get("/", async function(reg, res, next) {
+        try {
+            const products = await Promise.resolve(productsMock);
+
+            res.status(200).json({
+                data: products,
+                message: 'products listed'
+            });
+        }catch(err){
+            next(err);
+        }
+    });
+}
+
+module.exports = productsApi;
 

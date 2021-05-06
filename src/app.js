d731be223914beca78add2a7991f02b6d5ad8021
mr-m33s33ks <briansanchezizq@gmail.com>
2021-05-05 22:14:20 -0500

    Products and categories api routes implementation within express app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 
+//import routes
+const productsApiRouter = require('./routes/api/products')
+const categoriesApiRouter = require('./routes/api/categories')
+
 function createApp() { 
   const app = express();
   app.use(cors());
   app.use(express.json());
 
   // ADD YOUR ROUTES
+  app.use("/api/products", productsApiRouter)
+  app.use("/api/categories", categoriesApiRouter)
   return app;
 }
 

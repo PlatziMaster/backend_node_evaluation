3fa115aea81c63ba2c5cf0cd43033f947ad7ccc9
mr-m33s33ks <briansanchezizq@gmail.com>
2021-05-05 22:12:06 -0500

    define categories api routes

---
+++ src/routes/api/categories.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const apiCategoriesController = require('../../controllers/api/categories')
+
+router.get('/', apiCategoriesController.list )
+router.get('/:categoryId', apiCategoriesController.show )
+router.post('/', apiCategoriesController.store )
+router.put('/:categoryId', apiCategoriesController.update )
+router.delete('/:categoryId', apiCategoriesController.destroy )
+router.get('/:categoryId/products', apiCategoriesController.listProducts )
+
+module.exports = router

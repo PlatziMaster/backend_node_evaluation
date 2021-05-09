d7b34705f016ad1dc6d6896240cd4e5307f8d3fe
Santiago Vargas Vega <santiagovargasve@gmail.com>
2021-05-09 09:25:57 -0500

    ADD:Connection to database

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-
-function createApp() { 
+const express = require("express");
+const cors = require("cors");
+const { connect } = require("./lib/mongo");
+function createApp() {
   const app = express();
   app.use(cors());
   app.use(express.json());
-
+  app.use(express.urlencoded({ extended: false }));
+  connect();
   // ADD YOUR ROUTES
   return app;
 }

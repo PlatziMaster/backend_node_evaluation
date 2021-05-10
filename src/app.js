8331825d7f3b03e42b7bc7616020be10ccb386b2
Hugo Lara <hugo.alex200197@outlook.com>
2021-05-09 23:13:52 -0500

    Generacion de servidor y conexion a base de datos con mongo

--- src/app.js
+++ src/app.js
@@ -1,13 +1,45 @@
+const path = require('path');
 const express = require('express');
-const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-function createApp() { 
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
+//const cors = require('cors');
+const app = express();
 
-  // ADD YOUR ROUTES
-  return app;
-}
+//Connecting DB
+mongoose.connect('mongodb://localhost/crud-mongo')
+  .then(db => console.log('Db connected'))
+  .catch(err => console.log(err));
+  
 
-module.exports = createApp;
+//Importing Routes
+const indexRoutes = require('./routes/index');
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`);
+});
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+
+//routes
+app.use('/', indexRoutes);
+
+
+// function createApp() { 
+//   const app = express();
+//   app.use(cors());
+//   app.use(express.json()); 
+
+//   // ADD YOUR ROUTES
+//   return app;
+// }
+
+// module.exports = createApp;
+  

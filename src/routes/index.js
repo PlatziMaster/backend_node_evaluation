d7f8d88220f319e7f6f1b4376f7dbbf95b8c3c47
Hugo Lara <hugo.alex200197@outlook.com>
2021-05-10 23:04:31 -0500

    Creacion de delete y update

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,40 @@ const router = express.Router();
 
 const Product = require('../models/product.js');
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', async (req, res) => {
+    const product =  await Product.find();
+    console.log(product);
+    res.render('index', {
+        product
+    });
 });
 
-router.post('/add', (req, res) =>{
-    console.log(new Product());
-    console.log(req.body);
-    res.send('Received');
+router.post('/add', async (req, res) =>{
+    const product =  new Product(req.body);
+    await product.save();
+    res.redirect('/');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('edit', {
+        product
+    });
+});
+
+router.post('/edit/:id', async(req, res) => {
+    const { id } = req.params;
+    await Product.update({_id: id}, req.body);
+    res.redirect('/');
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await Product.remove({_id: id});
+    res.redirect('/');
 });
 
 module.exports = router;
+
 

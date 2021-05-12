50bfe0fab6268f0774dc93522b5af95e20852dd2
jscoder <javier.ant.sanchezc@gmail.com>
2021-05-12 11:03:51 -0500

    commit de conexion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const { config } = require('./config');
 const createApp = require('./app');
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 
 const app = createApp();
 
 app.listen(config.port, err => {
+
+  mongoose.connect('mongodb+srv://platzimaster:platzimaster@platzimaster.ijdub.mongodb.net/test')
+.then(db => console.log('db connected'))
   if (err) {
     console.error("Error: ", err);
     return;

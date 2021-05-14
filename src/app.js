b473205933173fda89fb99691c9d3d45001e8134
Javier Ledezma <juls0593@gmail.com>
2021-05-13 21:56:19 -0500

    Install and initialize mongoose

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('../src/config').config;
 
-function createApp() { 
+function createApp() {
   const app = express();
   app.use(cors());
   app.use(express.json());
+  mongoose.connect(
+      `mongodb://${config.dbHost}:${config.dbPort}/${config.dbName}`,
+    {useNewUrlParser: true, useUnifiedTopology: true}
+  );
 
   // ADD YOUR ROUTES
   return app;

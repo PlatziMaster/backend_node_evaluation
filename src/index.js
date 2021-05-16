3a7d612ddf438e274a1e47007c2cae5c6edb46dc
Carlos Sanchez <carlos.david.sanchez.moreno@gmail.com>
2021-05-16 13:59:57 -0600

    Fix at running server in production

--- src/index.js
+++ src/index.js
@@ -13,16 +13,17 @@ app.get('/', function(req, res) {
 });
 
 // Connection with mongo DB and express initialization.
-mongoose.connect(`${config.dbConnection}://${config.dbHost}:${config.dbPort}/${config.dbName}`,  {
+const DB_URL = `${config.dbConnection}://${config.dbHost}:${config.dbPort}/${config.dbName}`;
+mongoose.connect(DB_URL,  {
         "auth": { "authSource": "admin" },
         "user": config.dbUser,
         "pass": config.dbPassword,
-        "u1seNewUrlParser": true,
+        "useNewUrlParser": true,
     }, (err, res) => {
         if(err) throw err
         console.log('DB Connected.')
         app.listen(config.port, err => {
-            console.log('Server listening at port 3000.')
+            console.log(`Server listening at port ${config.port}`)
             if (err) {
             console.error("Error: ", err);
             return;

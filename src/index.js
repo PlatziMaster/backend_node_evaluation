f6270a9c9a949dff68d797997d0b59ee6f055d5a
Javier Amaya Patricio <javieramayapat@gmail.com>
2021-05-16 11:48:10 -0500

    Deploy 3 in package fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const createApp = require('./app');
 
 const app = createApp();
 
-app.listen(config.port, () => {
-  console.log(`La aplicación esta escuchando en el puerto http://localhost:${config.port}`);
+app.listen(config.port, err => {
+  if (err) {
+    console.error("Error: ", err);
+    return;
+  }
 });

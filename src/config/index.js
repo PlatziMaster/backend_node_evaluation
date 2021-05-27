f83f11e35e502584ae97b09d94465c38d04a0939
Daniel Herrera Romano <daniel.herrerarom@gmail.com>
2021-05-27 02:27:01 -0500

    Create - Creation of controllers and env

--- src/config/index.js
+++ src/config/index.js
@@ -8,7 +8,7 @@ const config = {
   dbHost: process.env.MONGO_HOST,
   dbName: process.env.MONGO_DB_NAME,
   dbPort: process.env.MONGO_PORT,
-  dbConnection: process.env.MONGO_CONNECTION,
+  dbConnection: process.env.MONGO_CONNECTION
 };
 
 module.exports = { config };

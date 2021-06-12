faffba641e7396d950d75a7cfc8b1fda54961148
Alex <alexyami27@gmail.com>
2021-06-11 22:28:12 -0500

    Test

---
+++ app.js
@@ -0,0 +1,5 @@
+// server.js
+console.log(`Your port is ${process.env.PORT}`); // undefined
+const dotenv = require('dotenv');
+dotenv.config();
+console.log(`Your port is ${process.env.PORT}`); // 3000

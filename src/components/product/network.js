8c0379f83caea47f7ba6cf34742daca25a21c8e3
Hugo Lara <hugo.alex200197@outlook.com>
2021-07-12 15:55:06 -0500

    Se creo carpeta componentes para tener separado codigo

---
+++ src/components/product/network.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const multer = require('multer');
+const router = express.Router();
+
--- src/models/product.js
+++ src/models/product.js
@@ -8,9 +8,4 @@ const productSchema = new Schema({
     categoryId: Number
 });
 
-const categorySchema = new Schema({
-    name: String,
-});
-
 module.exports =  mongoose.model('product', productSchema);
-module.exports =  mongoose.model('category', categorySchema);

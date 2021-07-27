cd9eff48f2e88ec979f881dfce2c48e5aa995c14
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-07-27 15:57:46 -0500

    :recycle: refactor(routes): implement routes builder in resources

--- src/routes/api/categories.ts
+++ src/routes/api/categories.ts
@@ -1,103 +1,17 @@
-import { Express, NextFunction, Request, Response, Router } from 'express'
+import { Express } from 'express'
 import CategoriesService from '../../services/resources/categories'
-import HttpResponse from '../../network/response'
+import buildRoutes from './builder'
 
-const router = Router()
+const resource = 'categories'
+const service = new CategoriesService(resource)
 
 /**
  * Set all CRUD endpoints for the resource.
  *
  * @param app - Express application.
  */
-const categories = (app: Express): void => {
-  const categoriesService = new CategoriesService('categories')
-
-  // create
-  router.post('/', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const data = req.body
-      const result = await categoriesService.create(data)
-
-      HttpResponse.success(res, result, 201)
-    } catch (err) {
-      next(err)
-    }
-  })
-
-  // read all
-  router.get('/', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const result = await categoriesService.read()
-
-      HttpResponse.success(res, result)
-    } catch (err) {
-      next(err)
-    }
-  })
-
-  // read one
-  router.get(
-    '/:id',
-    async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const id = req.params.id
-        const result = await categoriesService.find(id)
-
-        HttpResponse.success(res, result)
-      } catch (err) {
-        next(err)
-      }
-    }
-  )
-
-  // entire update
-  router.put(
-    '/:id',
-    async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const id = req.params.id
-        const data = req.body
-        const result = await categoriesService.update(id, data)
-
-        HttpResponse.success(res, result)
-      } catch (err) {
-        next(err)
-      }
-    }
-  )
-
-  // partial update
-  router.patch(
-    '/:id',
-    async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const id = req.params.id
-        const data = req.body
-        const result = await categoriesService.update(id, data)
-
-        HttpResponse.success(res, result)
-      } catch (err) {
-        next(err)
-      }
-    }
-  )
-
-  // delete
-  router.delete(
-    '/:id',
-    async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const id = req.params.id
-        const result = await categoriesService.delete(id)
-
-        HttpResponse.success(res, result)
-      } catch (err) {
-        next(err)
-      }
-    }
-  )
-
-  app.use('/api/categories', router)
+const routes = (app: Express): void => {
+  return buildRoutes(app, resource, service)
 }
 
-export default categories
+export default routes
--- src/routes/api/products.ts
+++ src/routes/api/products.ts
@@ -1,103 +1,17 @@
-import { Express, NextFunction, Request, Response, Router } from 'express'
+import { Express } from 'express'
 import ProductsService from '../../services/resources/products'
-import HttpResponse from '../../network/response'
+import buildRoutes from './builder'
 
-const router = Router()
+const resource = 'products'
+const service = new ProductsService(resource)
 
 /**
  * Set all CRUD endpoints for the resource.
  *
  * @param app - Express application.
  */
-const products = (app: Express): void => {
-  const productsService = new ProductsService('products')
-
-  // create
-  router.post('/', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const data = req.body
-      const result = await productsService.create(data)
-
-      HttpResponse.success(res, result, 201)
-    } catch (err) {
-      next(err)
-    }
-  })
-
-  // read all
-  router.get('/', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const result = await productsService.read()
-
-      HttpResponse.success(res, result)
-    } catch (err) {
-      next(err)
-    }
-  })
-
-  // read one
-  router.get(
-    '/:id',
-    async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const id = req.params.id
-        const result = await productsService.find(id)
-
-        HttpResponse.success(res, result)
-      } catch (err) {
-        next(err)
-      }
-    }
-  )
-
-  // entire update
-  router.put(
-    '/:id',
-    async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const id = req.params.id
-        const data = req.body
-        const result = await productsService.update(id, data)
-
-        HttpResponse.success(res, result)
-      } catch (err) {
-        next(err)
-      }
-    }
-  )
-
-  // partial update
-  router.patch(
-    '/:id',
-    async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const id = req.params.id
-        const data = req.body
-        const result = await productsService.update(id, data)
-
-        HttpResponse.success(res, result)
-      } catch (err) {
-        next(err)
-      }
-    }
-  )
-
-  // delete
-  router.delete(
-    '/:id',
-    async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const id = req.params.id
-        const result = await productsService.delete(id)
-
-        HttpResponse.success(res, result)
-      } catch (err) {
-        next(err)
-      }
-    }
-  )
-
-  app.use('/api/products', router)
+const routes = (app: Express): void => {
+  return buildRoutes(app, resource, service)
 }
 
-export default products
+export default routes

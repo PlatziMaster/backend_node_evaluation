2c3f70f12a1f1eb2c32c83e3fa2ff742175200e6
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-07-27 10:56:08 -0500

    :sparkles: chore(services): create a base resource service class

---
+++ src/services/resources/base-resource-service.ts
@@ -0,0 +1,62 @@
+import { Document } from 'mongodb'
+import Store from '../store/mongodb'
+
+class BaseResourceService {
+  store
+
+  constructor(resource: string) {
+    this.store = new Store(resource)
+  }
+
+  async create(data: Document): Promise<Document | undefined> {
+    try {
+      const result = await this.store.create(data)
+
+      return result
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async read(): Promise<Document | undefined> {
+    try {
+      const result = await this.store.read()
+
+      return result
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async find(id: string): Promise<Document | undefined> {
+    try {
+      const result = await this.store.find(id)
+
+      return result
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async update(id: string, data: Document): Promise<Document | undefined> {
+    try {
+      const result = await this.store.update(id, data)
+
+      return result
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async delete(id: string): Promise<Document | undefined> {
+    try {
+      const result = await this.store.delete(id)
+
+      return result
+    } catch (err) {
+      throw err
+    }
+  }
+}
+
+export default BaseResourceService
